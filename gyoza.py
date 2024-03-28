62940bbb1f987a435d8db3e88568af9f18d50304
Jack <jack.nguyen@techvify.com.vn>
2024-03-28 10:53:19 +0700

    update: found author link; using v3.2 as default due to baked vae

--- gyoza.py
+++ gyoza.py
@@ -3,12 +3,18 @@
 from .base_model import BaseModelCkpt, BooruPromptGenerator 
 
 ALL_MODEL_OPTIONS = {
-    "linges": "https://huggingface.co/mdl-mirror/Store-Bought-Gyoza/blob/main/LingesGyoza.safetensors",
-    "store": "https://huggingface.co/mdl-mirror/Store-Bought-Gyoza/blob/main/storeBoughtGyozaMix_storeboughtgyozav10.safetensors",
-    "flat": "https://huggingface.co/mdl-mirror/Store-Bought-Gyoza/blob/main/storeBoughtGyozaMix_flatgyozaafterdark.safetensors",
-    "evening": "https://huggingface.co/mdl-mirror/Store-Bought-Gyoza/blob/main/storeBoughtGyozaMix_eveninggyozasnackv10.safetensors"
+    "mdl_linges": "https://huggingface.co/mdl-mirror/Store-Bought-Gyoza/blob/main/LingesGyoza.safetensors",
+    "mdl_store": "https://huggingface.co/mdl-mirror/Store-Bought-Gyoza/blob/main/storeBoughtGyozaMix_storeboughtgyozav10.safetensors",
+    "mdl_flat": "https://huggingface.co/mdl-mirror/Store-Bought-Gyoza/blob/main/storeBoughtGyozaMix_flatgyozaafterdark.safetensors",
+    "mdl_evening": "https://huggingface.co/mdl-mirror/Store-Bought-Gyoza/blob/main/storeBoughtGyozaMix_eveninggyozasnackv10.safetensors",
+    "default": "https://huggingface.co/Jemnite/GyoZanMix/blob/main/factorygyozan/StoreGyoza.safetensors",
+    "v2": "https://huggingface.co/Jemnite/GyoZanMix/blob/main/factorygyozan/GyozaMixV2.safetensors",
+    "v3": "https://huggingface.co/Jemnite/GyoZanMix/blob/main/factorygyozan/GyozaMixV3fp16.safetensors",
+    "v3.2": "https://huggingface.co/Jemnite/GyoZanMix/blob/main/factorygyozan/GyozaMixV3fp16.safetensors",
+    "v3.5": "https://huggingface.co/Jemnite/GyoZanMix/blob/main/factorygyozan/GyozaMixV3.5.safetensors",
+    "v5": "https://huggingface.co/Jemnite/GyoZanMix/blob/main/factorygyozan/GyozaMixV5.safetensors"
 }
 
 class GyozaModel(BaseModelCkpt, BooruPromptGenerator):
-    def __init__(self, mode="store", **kwargs):
+    def __init__(self, mode="v3.2", **kwargs):
         super(GyozaModel, self).__init__(ALL_MODEL_OPTIONS[mode], vae_path=None, **kwargs)
