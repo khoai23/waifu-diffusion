95327f45c86bb35de8aca52eb141a72bfbc8eddf
Jack <jack.nguyen@techvify.com.vn>
2024-03-28 10:41:28 +0700

    update: all 4 variation of gyoza, testing now

--- gyoza.py
+++ gyoza.py
@@ -2,8 +2,13 @@
 
 from .base_model import BaseModelCkpt, BooruPromptGenerator 
 
-MODEL_PATH = "https://huggingface.co/mdl-mirror/Store-Bought-Gyoza/blob/main/LingesGyoza.safetensors"
+ALL_MODEL_OPTIONS = {
+    "linges": "https://huggingface.co/mdl-mirror/Store-Bought-Gyoza/blob/main/LingesGyoza.safetensors",
+    "store": "https://huggingface.co/mdl-mirror/Store-Bought-Gyoza/blob/main/storeBoughtGyozaMix_storeboughtgyozav10.safetensors",
+    "flat": "https://huggingface.co/mdl-mirror/Store-Bought-Gyoza/blob/main/storeBoughtGyozaMix_flatgyozaafterdark.safetensors",
+    "evening": "https://huggingface.co/mdl-mirror/Store-Bought-Gyoza/blob/main/storeBoughtGyozaMix_eveninggyozasnackv10.safetensors"
+}
 
 class GyozaModel(BaseModelCkpt, BooruPromptGenerator):
-    def __init__(self, **kwargs):
-        super(GyozaModel, self).__init__(MODEL_PATH, vae_path=None, **kwargs)
+    def __init__(self, mode="store", **kwargs):
+        super(GyozaModel, self).__init__(ALL_MODEL_OPTIONS[mode], vae_path=None, **kwargs)
